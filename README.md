b061806e612a107198735aac756dbe5f1188dcbb
mijorus <lorenzo.mijorus@gmail.com>
2021-09-16 21:57:57 +0200

    improved overall script and added table with most used severs

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Speeedtest Graph
 
-A simple speedtest graph to run with a cronjob
+This is a pretty simple python script that will output an html file with a graph showing your connection speed history.
+It was develped to be used in a cronjob on a Raspberry Pi running Raspbian Buster, but it is very simple code which should run on other systems as well.
 
 ## Requires
 ```python
 pip install iso8601
-mkdir public/
-touch public/history.html
-echo '[]' > database.json
 ```
+## Usage
+```sh
+python3 speedtest-cron.py
+```
+
+By default the script will sleep for a random amount of secods (less than a minute don't worry) in order to avoid high traffic on Speedtest.net's servers and possibly a failure of the test.
+
+You can override this behavior with
+```sh
+python3 speedtest-cron.py --no-sleep
+``` 
--- speedtest-cron.py
+++ speedtest-cron.py
@@ -1,59 +1,81 @@
-from os import openpty
 import subprocess
 import json
 import re
-from datetime import datetime
 import iso8601
 import pathlib
 import pytz
 import time
 import random
+import os
+import sys
+
+if not '--no-sleep' in sys.argv:
+    time.sleep(random.randint(10, 40))
 
 path = str(pathlib.Path(__file__).parent.resolve())
 
-database = json.loads(open((path +'/database.json'), 'rt').read())
-
-time.sleep(random.randint(10, 40))
+def db_create(content, filename = 'database.json'):
+    f = open((path +'/database/'+filename), 'w')
+    f.write(json.dumps(content))
+    f.close()
 
 try:
-	speed_test = json.loads(subprocess.check_output(['/usr/local/bin/speedtest', '--json']))
+    database = json.loads(open((path +'/database/database.json'), 'rt').read())
+except:
+    database = []
+
+db_create(database, 'database_backup.json')
+
+command = subprocess.check_output('which speedtest', shell=True).decode().strip()
+
+try:
+    speed_test = json.loads(subprocess.check_output([command, '--json']))
 except subprocess.CalledProcessError as e:
-        speed_test = {'download': 0, 'upload': 0, 'ping': 0}
+    speed_test = {'download': 0, 'upload': 0, 'ping': 0}
 
 database.append(speed_test)
-
-f = open((path +'/database.json'), 'w')
-f.write(json.dumps(database))
-f.close()
+db_create(database)
 
 template = open((path +'/template.hbs'), 'rt')
 
 labels = []
+downloads = []
+uploads = []
+pings = []
+servers = {}
 for data in database:
     if 'timestamp' in data:
         tz = pytz.timezone('Europe/Rome')
-        labels.append(iso8601.parse_date(data['timestamp']).astimezone(tz).strftime("%d/%m/%Y"))
-
-downloads = []
-for data in database:
+        labels.append(iso8601.parse_date(data['timestamp']).astimezone(tz).strftime("%d/%m/%Y %H:%M"))
+    
     if 'download' in data:
         downloads.append( int(data['download']) / 1000000 )
 
-uploads = []
-for data in database:
     if 'upload' in data:
         uploads.append( int(data['upload']) / 1000000 )
 
-pings = []
-for data in database:
     if data['ping']:
         pings.append(data['ping'])
+
+    if 'server' in data:
+        server = servers[data['server']['id']] if data['server']['id'] in servers else {}
+        servers[data['server']['id']] = {
+            'name': data['server']['name'] + ' ' + (data['server']['sponsor'] if 'sponsor' in data['server'] else ''), 
+            'count': 1 if not 'count' in server else server['count'] + 1
+        }
 
 html = template.read()
 html = re.sub('{{ labels }}', json.dumps(labels), html)
 html = re.sub('{{ downloads }}', json.dumps(downloads), html)
 html = re.sub('{{ uploads }}', json.dumps(uploads), html)
 html = re.sub('{{ pings }}', json.dumps(pings), html)
+
+s_table = ''
+for server_id, s in servers.items():
+    s_table = f'{s_table} <tr><td>{server_id}</td><td>{s["name"]}</td><td>{s["count"]}</td></tr>'
+
+html = re.sub('{{ servers_table }}', s_table, html)
+
 template.close()
 
 output = open((path +'/public/history.html'), 'w')
